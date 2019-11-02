c8acf7bee46d2b8bafc64bcc7395f83df537d395
guy.golan <guygolan91@gmail.com>
2019-11-02 14:11:11 +0200

    added ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,6 +5,6 @@ export const BuildControl = (props) => (
     <div className="BuildControl">
         <div className="Label">{props.label}</div>
         <button className="Less">Less</button>
-        <button className="More">More</button>
+        <button onClick={props.added} className="More">More</button>
     </div>
 );
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 
 import "./BuildControls.css";
 
 //Components
-import { BuildControl } from './BuildControl/BuildControl';
+import { BuildControl } from "./BuildControl/BuildControl";
 
 const controls = [
-    { label: 'Salad', type: 'salad'},
-    { label: 'Bacon', type: 'bacon'},
-    { label: 'Cheese', type: 'cheese'},
-    { label: 'Meat', type: 'meat'},
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
 ];
 
-export const BuildControls = (props) => (
-    <div className="BuildControls">
-        {controls.map(control => <BuildControl key={control.label} label={control.label} />)}
-    </div>
+export const BuildControls = props => (
+  <div className="BuildControls">
+    {controls.map(control => {
+      return (
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => {
+            props.ingredientAdded(control.type);
+          }}
+        />
+      );
+    })}
+  </div>
 );
